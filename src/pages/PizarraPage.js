2e05e86d1617d9c5b7df5f38a771464858fe6fa4
ddmrenovalpropiedades <ddm@renovalpropiedades.com>
2026-06-25 00:05:42 -0400

    Update PizarraPage.js

--- src/pages/PizarraPage.js
+++ src/pages/PizarraPage.js
@@ -1004,7 +1004,6 @@ export default function PizarraPage() {
                     />
                   );
                 })
-                ))
               }
             </tbody>
           </table>
