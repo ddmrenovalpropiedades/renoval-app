3b9d95666d08226e2766fb57e956d2b893fb7873
ddmrenovalpropiedades <ddm@renovalpropiedades.com>
2026-06-25 14:07:38 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,13 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Registrar Service Worker para habilitar instalación PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .catch((err) => console.error('Service Worker error:', err));
+  });
+}
+
 reportWebVitals();
